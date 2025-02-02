cbad8caf9b7b1e3ab189408377beae71aa6a2e25
Sonish Maharjan <sonishmaharjan1@gmail.com>
2025-02-03 01:14:20 +1030

    feat: setup project structure and added health api

---
+++ backend/api/tests/test_health_check.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class HeathCheckTest(SimpleTestCase):
+
+    def test_health_check(self):
+        """Test that health check return status ok JSON response."""
+        url = reverse('health_check')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {'status': 'ok'})
+
